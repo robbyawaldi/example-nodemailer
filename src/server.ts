6ae84743e6f4bef5b9efe57bd3060c125d8abe63
Robby Awaldi <robbyawaldi@gmail.com>
2021-09-28 07:11:04 +0700

    first init

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import nodemailer from "nodemailer";
+import fs from 'fs'
+import handlebars from "handlebars";
+import path from 'path'
+
+export async function sendEmail(to: string) {
+
+    const readHTMLFile = function (path: string, callback: Function) {
+        fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+            if (err) {
+                callback(err);
+                throw err;
+            }
+            else {
+                callback(null, html);
+            }
+        });
+    };
+
+    readHTMLFile(path.join(__dirname, "index.html"), async function (_: Error, html: any) {
+        const template = handlebars.compile(html);
+
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            requireTLS: true,
+            auth: {
+                user: "robbyawaldi@gmail.com", // user your email
+                pass: "", 
+            },
+            logger: true
+        });
+        
+        const htmlToSend = template({})
+
+        let info = await transporter.sendMail({
+            from: '"Example" <robbyawaldi@gmail.com>', // sender address
+            to: to, // list of receivers
+            subject: "Reset Password", // Subject line
+            html: htmlToSend,
+        });
+    
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    });
+}
+
+sendEmail("robbyawaldi@gmail.com")
